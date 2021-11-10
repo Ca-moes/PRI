409f497998b962ed068dfb4cd99f4dd9fc8ad883
andrenasx <andre2000.an@gmail.com>
2021-11-10 20:59:43 +0000

    Created Milestone1 folder

--- M1/processing/merge.py
+++ M1/processing/merge.py
@@ -20,7 +20,7 @@ items2021 = pd.read_csv("../data/20211108-items.csv", sep=",")
 all_items = pd.concat([items2021, items2019]).drop_duplicates(subset=['asin']).reset_index(drop=True)
 
 # Write merged items to file
-all_items.to_csv('../data/items.csv', index=False)
+all_items.to_csv('../data/merged_items.csv', index=False)
 
 
 # Same for reviews...
@@ -29,4 +29,4 @@ reviews2021= pd.read_csv("../data/20211108-reviews.csv", sep=",")
 
 all_reviews = pd.concat([reviews2021, reviews2019]).drop_duplicates(subset=['asin', 'name', 'title']).reset_index(drop=True)
 
-all_reviews.to_csv('../data/reviews.csv', index=False)
+all_reviews.to_csv('../data/merged_reviews.csv', index=False)
