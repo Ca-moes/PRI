07662e95462cd439a6f40ecafe21005be432716b
andrenasx <andre2000.an@gmail.com>
2021-11-13 20:09:34 +0000

    postgres db

--- M1/db/schema.sql
+++ M1/db/schema.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS Brand;
-DROP TABLE IF EXISTS Cellphone;
 DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS Cellphone;
+DROP TABLE IF EXISTS Brand;
 
 CREATE TABLE Brand (
-    id  INTEGER PRIMARY KEY,
-    name TEXT NOT NULL
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE Cellphone (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     asin TEXT UNIQUE NOT NULL,
-    brand INTEGER NOT NULL,
+    brand_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     url TEXT NOT NULL,
     image TEXT NOT NULL,
@@ -19,20 +19,20 @@ CREATE TABLE Cellphone (
     totalRatings INTEGER NOT NULL,
     price REAL NOT NULL,
     originalPrice REAL NOT NULL,
-    CONSTRAINT brand_fk FOREIGN KEY (brand) REFERENCES Brand (id) ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT price_check CHECK (price >=0 AND originalPrice >= 0)
+    CONSTRAINT brand_fk FOREIGN KEY(brand_id) REFERENCES Brand(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT price_check CHECK(price >=0 AND originalPrice >= 0)
 );
 
 CREATE TABLE Review (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     cellphone_id INTEGER NOT NULL,
     name TEXT NOT NULL,
     rating INT NOT NULL,
-    date TEXT NOT NULL,
-    verified TEXT NOT NULL,
+    date DATE NOT NULL,
+    verified BOOLEAN NOT NULL,
     title TEXT,
     body TEXT,
     helpfulVotes INTEGER NOT NULL DEFAULT 0,
     country TEXT NOT NULL,
-    CONSTRAINT asin_fk FOREIGN KEY (cellphone_id) REFERENCES Cellphone (id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT asin_fk FOREIGN KEY(cellphone_id) REFERENCES Cellphone(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
--- M1/scripts/seed.py
+++ M1/scripts/seed.py
@@ -14,17 +14,17 @@ with open("db/seed.sql", "w") as f:
 
     f.write("\n--CELLPHONES--\n")
     for index, row in items.iterrows():
-        brand = brand_dict[row['brand']]
+        brand_id = brand_dict[row['brand']]
         title = str(row['title']).replace("'", "''")
-        f.write(f"INSERT INTO Cellphone VALUES ({index+1}, '{row['asin']}', {brand}, '{title}', '{row['url']}', '{row['image']}', {row['rating']}, '{row['reviewUrl']}', {row['totalRatings']}, {row['price']}, {row['originalPrice']});\n")
+        f.write(f"INSERT INTO Cellphone VALUES ({index+1}, '{row['asin']}', {brand_id}, '{title}', '{row['url']}', '{row['image']}', {row['rating']}, '{row['reviewUrl']}', {row['totalRatings']}, {row['price']}, {row['originalPrice']});\n")
 
     f.write("\n--REVIEWS--\n")
     for index, row in reviews.iterrows():
         name = str(row['name']).replace("'", "''")
         title = str(row['title']).replace("'", "''")
         body = str(row['body']).replace("'", "''")
-        cellphone_id = items['asin'].eq(row['asin']).idxmax()
-        f.write(f"INSERT INTO Review VALUES ({index+1}, {cellphone_id}, '{name}', {row['rating']}, '{row['date']}', '{row['verified']}', '{title}', '{body}', {row['helpfulVotes']}, '{row['country']}');\n")
+        cellphone_id = items['asin'].eq(row['asin']).idxmax() + 1
+        f.write(f"INSERT INTO Review VALUES ({index+1}, {cellphone_id}, '{name}', {row['rating']}, '{row['date']}', {row['verified']}, '{title}', '{body}', {row['helpfulVotes']}, '{row['country']}');\n")
 
     f.close()
 
