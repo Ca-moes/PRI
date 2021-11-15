0cd46c6ed564c519a7efa86ab956fe5473fa64ae
Catarina Fernandes <up201806610@fe.up.pt>
2021-11-15 01:44:44 +0000

    Add review ratings distribution by brand

--- scripts/stats.py
+++ scripts/stats.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
+import seaborn as sns
+sns.set()
+
 def generate_wordcloud(df, name, column):
     text = ''
     stopwords = set(STOPWORDS)
@@ -34,9 +37,29 @@ def generate_wordcloud(df, name, column):
     # Save image
     plt.savefig('stats/' + name + '_' + column + '_wordcloud.png', bbox_inches='tight')
 
+
+def generate_dist_ratings_by_brand(items, brand):
+    # items_by_brand = items.groupby(items["brand"])["rating"].count()
+   # print(items.groupby(items["brand"])["rating"])
+   # pd.set_option('display.max_columns', None)
+   # print("Hey")
+    brand_items = items[items["brand"] == brand]
+    # print(items["brand"])
+    #print(brand_items["brand"])
+    #print(brand_items)
+    if brand_items.size > 0:
+        # we want the ratings from the reviews -> rating_y
+        brand_items["rating_y"].value_counts().sort_index().plot(kind='bar',color='#8c2d19', title=brand + ' reviews distribution')
+        plt.savefig('stats/' + brand + '_reviews_dist_bargraph.png', bbox_inches='tight')
+
+
+
+
 if __name__ == '__main__':
     items = pd.read_csv('data/items_clean.csv')
     reviews = pd.read_csv('data/reviews_clean.csv')
+
+    merged_data = pd.merge(items, reviews, on='asin', how='inner')
 
     items['brand'].value_counts().plot(kind='bar',color='#8c2d19', title='Number of cellphones per brand')
     plt.savefig('stats/items_brand_bargraph.png', bbox_inches='tight')
@@ -44,6 +67,23 @@ if __name__ == '__main__':
     reviews['country'].value_counts().plot(kind='bar',color='#8c2d19', title='Number of reviews per country')
     plt.savefig('stats/reviews_country_bargraph.png', bbox_inches='tight')
 
-    generate_wordcloud(items, 'items', 'title')
-    generate_wordcloud(reviews, 'reviews', 'title')
-    generate_wordcloud(reviews, 'reviews', 'body')
+    reviews['rating'].value_counts().sort_index().plot(kind='bar',color='#8c2d19', title='Review distribution globally')
+    plt.savefig('stats/review_dist_globally_bargraph.png', bbox_inches='tight')
+
+    generate_dist_ratings_by_brand(merged_data, 'ASUS')
+    generate_dist_ratings_by_brand(merged_data, 'Apple')
+    generate_dist_ratings_by_brand(merged_data, 'Google')
+    generate_dist_ratings_by_brand(merged_data, 'HUAWEI')
+    generate_dist_ratings_by_brand(merged_data, 'Motorola')
+    generate_dist_ratings_by_brand(merged_data, 'Nokia')
+    generate_dist_ratings_by_brand(merged_data, 'OnePlus')
+    generate_dist_ratings_by_brand(merged_data, 'Samsung')
+    generate_dist_ratings_by_brand(merged_data, 'Sony')
+    generate_dist_ratings_by_brand(merged_data, 'Xiaomi')
+
+    # generate_wordcloud(items, 'items', 'title')
+    # generate_wordcloud(reviews, 'reviews', 'title')
+    # generate_wordcloud(reviews, 'reviews', 'body')
+
+
+
