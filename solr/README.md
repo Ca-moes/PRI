aa72f7f315e931eeaa92274a3ae9dd29dd594d35
Dani <andre3000pt@gmail.com>
2021-12-07 15:14:11 +0000

    coisas 👏

--- solr/README.md
+++ solr/README.md
@@ -1,3 +1,7 @@
+- Fazer o ficheiro executavel com `chmod +x startup.sh`
+
 docker build . -t amazon_solr
 
 docker run --name amazon_reviews -p 8983:8983 -v ${PWD}/data:/data --rm amazon_solr
+
+http://localhost:8983/
--- solr/startup.sh
+++ solr/startup.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+echo "--- precreate-core amazon"
 precreate-core amazon
 
 # Start Solr in background mode so we can use the API to upload the schema
+echo "--- solr start"
 solr start
 
-sleep 3
+echo "--- sleep 5"
+sleep 5
 
 # Schema definition via API
+echo "--- curl schema"
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/schema.json \
     http://localhost:8983/solr/amazon/schema
 
 # Populate collection
+echo -e "\n--- curl data"
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/data.json \
     'http://localhost:8983/solr/amazon/update/json/docs?split=/|/reviews'
 
 # Restart in foreground mode so we can access the interface
+echo -e "\n--- solr restart -f"
 solr restart -f
