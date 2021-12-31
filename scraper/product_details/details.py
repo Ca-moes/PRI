1247d81e08b7446353cadf91b070f82d8e428804
André Nascimento <andre2000.an@gmail.com>
2021-12-31 18:50:40 +0000

    Finished product details scrapping

--- scraper/product_details/details.py
+++ scraper/product_details/details.py
@@ -1,23 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-from random import randint
+from random import randint, choice
 from time import sleep
 import csv
 
-#! https://www.amazon.com/Motorola-Moto-Factory-Unlocked-Phone/dp/B074VFRKZG
+user_agent_list = [
+#'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+#'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+#'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+#'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+#'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'
+]
 
 def scrape(url):
+    user_agent = choice(user_agent_list)
+
     headers = {
         'dnt': '1',
         'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+        'user-agent': user_agent,
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'sec-fetch-site': 'same-origin',
         'sec-fetch-mode': 'navigate',
         'sec-fetch-user': '?1',
         'sec-fetch-dest': 'document',
         'referer': 'https://www.amazon.com/',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'
     }
 
     # Download the page using requests
@@ -39,12 +52,15 @@ with open('urls.txt', 'r') as urllist, open('items_details.csv', 'w', newline=''
     writer = csv.DictWriter(outfile, fieldnames=csv_columns)
     writer.writeheader()
 
-    for url in urllist.read().splitlines():
+    for asin_url in urllist.read().splitlines():
+        asin, url = asin_url.split(",")
+
         sleep(randint(1,5))
+
         soup = BeautifulSoup(scrape(url), "html.parser")
         if soup:
             details = {}
-            details['asin'] = url.rsplit('/', 1)[-1]
+            details['asin'] = asin
 
             try:
                 details['description'] = soup.find(id="productDescription").p.span.find(text=True, recursive=True).strip()
