df93b5de295d070c41b14f15f93a3aac8518b225
Dani <andre3000pt@gmail.com>
2022-01-05 23:07:57 +0000

    axios works

--- web/frontend/src/AppTheme.tsx
+++ web/frontend/src/AppTheme.tsx
@@ -2,19 +2,19 @@ import { createTheme } from "@mui/material/styles";
 import darkScrollbar from "@mui/material/darkScrollbar";
 
 const theme = createTheme({
-    palette: {
-        mode: "light",
-    },
-    typography: {
-        fontFamily: ["Poppins", "Roboto", "sans-serif"].join(","),
-    },
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: {
-                body: darkScrollbar(),
-            },
-        },
-    },
+    // palette: {
+    //     mode: "dark",
+    // },
+    // typography: {
+    //     fontFamily: ["Poppins", "Roboto", "sans-serif"].join(","),
+    // },
+    // components: {
+    //     MuiCssBaseline: {
+    //         styleOverrides: {
+    //             body: darkScrollbar(),
+    //         },
+    //     },
+    // },
 });
 
 export default theme;
--- web/frontend/src/pages/HomePage.tsx
+++ web/frontend/src/pages/HomePage.tsx
@@ -1,6 +1,23 @@
 import Button from "@mui/material/Button";
+import { AxiosResponse } from "axios";
+const axios = require('axios');
+
 
 const HomePage = () => {
+  // Make a request for a user with a given ID
+  axios.get('http://localhost:3001/test')
+  .then(function (response: AxiosResponse) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error: any) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
   return (
     <>
       <div>HomePage</div>
