56fd816e9432e6f6809500075488a16736f5caae
André Nascimento <andre2000.an@gmail.com>
2022-01-07 22:45:48 +0000

    Fix Logo src

--- web/frontend/src/components/layout/Header.tsx
+++ web/frontend/src/components/layout/Header.tsx
@@ -8,7 +8,6 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
 const pages = ['Phones', 'Reviews', 'About Us'];
@@ -29,7 +28,7 @@ const ResponsiveAppBar = () => {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
-            <img src="./pri-logo-white.png" alt="Phone Prime Logo" height={40} />
+            <img src="/pri-logo-white.png" alt="Phone Prime Logo" height={40} />
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -74,7 +73,7 @@ const ResponsiveAppBar = () => {
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
           >
-            <img src="./pri-logo-white.png" alt="Phone Prime Logo" height={40} />
+            <img src="/pri-logo-white.png" alt="Phone Prime Logo" height={40} />
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
