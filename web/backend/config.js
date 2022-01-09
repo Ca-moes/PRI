04300a157649438b79e40aa4eccc056142f287c3
André Nascimento <andre2000.an@gmail.com>
2022-01-09 22:37:25 +0000

    paramsSerializer

--- web/backend/config.js
+++ web/backend/config.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
+const Qs = require("qs");
 
 const core = axios.create({
   baseURL: 'http://localhost:8983/solr/amazon',
-  timeout: 1000
+  timeout: 1000,
+  paramsSerializer: params => Qs.stringify(params, {arrayFormat: 'repeat'})
 });
 
 module.exports = core;
