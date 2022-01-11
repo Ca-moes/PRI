4c272b1c0d9798777533dc1564c4f184efb87c1b
Catarina Fernandes <up201806610@fe.up.pt>
2022-01-11 14:11:09 +0000

    Removed footer

--- web/frontend/src/components/layout/PageLayout.jsx
+++ web/frontend/src/components/layout/PageLayout.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 import Header from "./Header";
-import Footer from "./Footer";
 
 const PageLayout = ({children}) => {
   return (
     <>
       <Header/>
       {children}
-      <Footer/>
     </>
   );
 };
